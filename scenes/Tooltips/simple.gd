7f5672a4dd8423869797eb54bf7b62961d6fc423
CraftLab <mateuszstolarczyk2010@outlook.com>
2026-06-15 19:52:11 +0200

    Automatyczny zapis
    Data: 15.06.2026 19:52:11,59

--- scenes/Tooltips/simple.gd
+++ scenes/Tooltips/simple.gd
@@ -8,6 +8,3 @@ func _process(_delta: float) -> void:
 		# 2. Odejmujemy 1.0 piksel, żeby strzałka minimalnie "wjechała" na panel i zlikwidowała szparkę.
 		#    (Jeśli po testach nadal zobaczysz mikro-szparkę, zmień - 1.0 na - 2.0).
 		arrow_anchor.position.x = self.size.x + 4.3
-		
-		# 3. Zaokrąglamy też pozycję Y, żeby pionowa krawędź nie dostała ułamków i nie była rozmyta.
-		arrow_anchor.position.y = round(arrow_anchor.position.y)
