43a231f97bdc758c55d7d5e557afe9bd054ad77f
CraftLab <mateuszstolarczyk2010@outlook.com>
2026-06-15 19:47:07 +0200

    Automatyczny zapis
    Data: 15.06.2026 19:47:06,53

--- scenes/Tooltips/simple.gd
+++ scenes/Tooltips/simple.gd
@@ -1,10 +1,13 @@
 extends PanelContainer
 
-func _ready() -> void:
-	# Ustawiamy wysoki priorytet. Dzięki temu ten skrypt wykona się PO tym,
-	# jak skrypt-manager z innej sceny ustawi pozycję chmurki.
-	process_priority = 100
+@onready var arrow_anchor: Control = $ArrowAnchor/Arrow
 
 func _process(_delta: float) -> void:
-	# Teraz na pewno zaokrąglamy pozycję jako ostatni!
-	global_position = global_position.round()
+	if arrow_anchor:
+		# 1. Bierzemy pełną szerokość chmurki (self.size.x), co daje nam idealnie prawą krawędź.
+		# 2. Odejmujemy 1.0 piksel, żeby strzałka minimalnie "wjechała" na panel i zlikwidowała szparkę.
+		#    (Jeśli po testach nadal zobaczysz mikro-szparkę, zmień - 1.0 na - 2.0).
+		arrow_anchor.position.x = self.size.x + 4.3
+		
+		# 3. Zaokrąglamy też pozycję Y, żeby pionowa krawędź nie dostała ułamków i nie była rozmyta.
+		arrow_anchor.position.y = round(arrow_anchor.position.y)
